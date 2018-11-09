8ca85032eae5ef20427241a3b2832e9c833bac87
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-09 22:52:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js'),
     bot = new Discord.Client({sisableEveryone: true})
     console.log("muuuuuuuuute,");
 bot.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("509745217285652513");
+const mohamed= member.guild.channels.get("507935307543347203");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`**- Welcome To ,Magic**`), 4000)        
+setTimeout(() => mohamed.send(`**- Welcome To ,Paranoia .. **`), 4000)        
 }
 });
 bot.login(process.env.BOT_TOKEN)
