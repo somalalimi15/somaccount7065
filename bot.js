10151ed7ba61857e610c3f050593f4cda1e5b18c
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-09 23:18:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,12 +5,12 @@ client.on('guildMemberAdd', member => {
 const mohamed= member.guild.channels.get("507935307543347203");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`**- Welcome To ,Paranoia .. **`), 2000)        
+setTimeout(() => mohamed.send(`**- Welcome To ,Paranoia .. ❦ ' ♪' **`), 2000)        
 }
 });
 
 client.on('ready', () => {
-    client.channels.find(c => c.id === '507935307543347207').join();
+    client.channels.find(c => c.id === '507957578194485279').join();
 });
 
 client.login(process.env.BOT_TOKEN)
